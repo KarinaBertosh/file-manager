8cd16e7c3384e99d0280638d7eb235cedf76e608
KarinaBertosh <cherrycake246@gmail.com>
2023-06-20 17:41:26 +0300

    refactor: nwd.js

--- src/operations/nwd.js
+++ src/operations/nwd.js
@@ -1,8 +1,20 @@
 import { dirname, isAbsolute, resolve } from "path";
 import fs from "fs";
 
-export function up(path, rootDit) {
-  if (path === rootDit) {
+let file;
+
+const checkFileExist = (path, currentDir) => {
+  if (isAbsolute(path)) {
+    file = path;
+  } else {
+    const currentPath = resolve(currentDir, path);
+    file = currentPath;
+  }
+};
+
+export function up(path, currentDir) {
+  if (path === currentDir) {
+    console.log("Error: You are in the root folder");
     return path;
   } else {
     const result = dirname(path);
@@ -10,45 +22,39 @@ export function up(path, rootDit) {
   }
 }
 
-export function cd(command, currentDir) {
-  let path;
-  if (isAbsolute(command)) {
-    path = command;
-  } else {
-    const currentPath = resolve(currentDir, command);
-    path = currentPath;
-  }
-  try {
-    if (fs.existsSync(path)) {
-      return `${path}`;
-    }
-  } catch (err) {
-    return "error";
-  }
+export function cd(path, currentDir) {
+  checkFileExist(path, currentDir);
+  if (fs.existsSync(file)) {
+    return `${file}`;
+  } else console.log("Error: No such file exists");
 }
 
-export function ls(path) {
+export async function ls(currentDir) {
   let dirArr = [];
   let fileArr = [];
 
-  fs.readdir(path, (err, files) => {
-    if (err) console.log("Error");
-    else {
-      files.forEach((file) => {
-        fs.stat(file, function (err, stats) {
-          if (stats.isFile()) {
-            fileArr.push(`${file}: file`);
-          } else {
-            dirArr.push(`${file}: directory`);
-          }
-          if (files[files.length - 1] === file) {
-            dirArr.sort();
-            fileArr.sort();
-            const result = dirArr.concat(fileArr);
-            console.log(result);
-          }
+  if (fs.existsSync(currentDir)) {
+    fs.readdir(currentDir, (err, files) => {
+      if (err)
+        console.log("Error: No such directory exists, or you are in a file");
+      else {
+        files.forEach((f) => {
+          fs.lstat(f, function (err, stats) {
+            if (stats.isFile()) {
+              fileArr.push(`${f}: file`);
+            } else {
+              dirArr.push(`${f}: directory`);
+            }
+            if (files[files.length - 1] === f) {
+              dirArr.sort();
+              fileArr.sort();
+              console.log(dirArr.concat(fileArr));
+            }
+          });
         });
-      });
-    }
-  });
+      }
+    });
+  } else {
+    console.log("0, Error: No such directory exists, or you are in a file");
+  }
 }
